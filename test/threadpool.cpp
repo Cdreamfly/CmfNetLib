1a637a5c73ecef55b92011fc5eaf97d232de04f4
Cdreamfly <2290660986@qq.com>
2022-05-31 16:54:41 +0800

    完成线程池

---
+++ test/threadpool.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Cmf on 2022/5/31.
+//
+
+#include <iostream>
+#include "NetLib/log/log.hpp"
+#include "NetLib/thread/threadpool.hpp"
+
+int main() {
+
+    ThreadPool *pool = new ThreadPool(4);
+    std::vector<std::future<int>> rets;
+    for (int i = 0; i < 100; i++) {
+        rets.emplace_back(std::move(pool->Commit([i]() -> int {
+            std::cout << "id:" << std::this_thread::get_id() << std::endl;
+            return i;
+        })));
+    }
+    for (auto &ret: rets) {
+        LOG_INFO("%d", ret.get());
+    }
+    delete pool;
+    return 0;
+}
