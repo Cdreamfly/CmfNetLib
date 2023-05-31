87c70a4433e52b38d9705fe9a4fcf5076530f643
Cdreamfly <2290660986@qq.com>
2023-06-01 00:23:53 +0800

    Creation project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-string removeTrailingZeros(string num)
-{
-    return num.substr(0, num.find_last_not_of('0') + 1);
-}
+
 int main()
 {
-    std::cout << removeTrailingZeros("1230100");
+    std::cout << "hello" << std::endl;
     return 0;
 }
