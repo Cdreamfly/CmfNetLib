1705b4929b9c6ed50960a718c8dd4e81ac1f18c0
Cdreamfly <2290660986@qq.com>
2023-10-22 01:18:18 +0800

    delete cstring.h

--- include/net/Socket.hpp
+++ include/net/Socket.hpp
@@ -4,6 +4,9 @@
 #include "net/SocketOps.hpp"
 #include "net/InetAddress.hpp"
 
+#include <netinet/tcp.h>
+#include <cstring>
+
 namespace cm {
 	namespace net {
 		class Socket : private NonCopyable {
@@ -14,7 +17,44 @@ namespace cm {
 
 			int fd() const { return sockFd_; }
 
-			void bindAddress(const InetAddress &addr) const { bindOrDie(sockFd_, addr.getSocketAddr()); }
+			void bindAddress(const InetAddress &addr) const { sockets::bindOrDie(sockFd_, addr.getSocketAddr()); }
+
+			void listen() const { sockets::listenOrDie(sockFd_); }
+
+			int accept(InetAddress &peerAddr) const {
+				sockaddr_in6 addr{};
+				memset(&addr, 0, sizeof(addr));
+				int connFd = sockets::accept(sockFd_, &addr);
+				if (connFd >= 0) {
+					peerAddr.setSocketAddr6(addr);
+				}
+				return connFd;
+			}
+
+			void shutdownWrite() const { sockets::shutdownWrite(sockFd_); }
+
+			void setTcpNoDelay(const bool on) const {
+				int opt = on ? 1 : 0;
+				::setsockopt(sockFd_, IPPROTO_TCP, TCP_NODELAY, &opt, static_cast<socklen_t>(sizeof(opt)));
+			}
+
+			void setReuseAddr(const bool on) const {
+				int opt = on ? 1 : 0;
+				::setsockopt(sockFd_, IPPROTO_TCP, SO_REUSEADDR, &opt, static_cast<socklen_t>(sizeof(opt)));
+			}
+
+			void setReusePort(const bool on) const {
+				int opt = on ? 1 : 0;
+				int ret = ::setsockopt(sockFd_, IPPROTO_TCP, SO_REUSEPORT, &opt, static_cast<socklen_t>(sizeof(opt)));
+				if (ret < 0 && on) {
+					LOG_FATAL("SO_REUSEPORT failed.");
+				}
+			}
+
+			void setKeepAlive(const bool on) const {
+				int opt = on ? 1 : 0;
+				::setsockopt(sockFd_, IPPROTO_TCP, SO_KEEPALIVE, &opt, static_cast<socklen_t>(sizeof(opt)));
+			}
 
 		private:
 			const int sockFd_;
