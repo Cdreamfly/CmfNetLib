6793cde57bf2084ff58f9a2a1270842aa3a82354
Cdreamfly <2290660986@qq.com>
2023-10-27 00:25:32 +0800

    add loop and quit loop

--- include/net/EventLoop.hpp
+++ include/net/EventLoop.hpp
@@ -1,13 +1,72 @@
 #pragma once
 
+#include <functional>
+#include <atomic>
+#include <memory>
+#include <mutex>
+
 #include "base/NonCopyable.hpp"
+#include "base/Timestamp.hpp"
+#include "base/CurrentThread.hpp"
 /**
  * 时间循环类，主要包含两大模块，Channel Poller(Epoll抽象)
  */
 namespace cm::net {
-	class EventLoo : private NonCopyable {
+	class Channel;
+
+	class Poller;
+
+	class EventLoop : private NonCopyable {
 	public:
+		using Functor = std::function<void()>;
+
+		EventLoop();
+
+		virtual ~ EventLoop();
+
+		void loop();
+
+		void quit();
+
+		[[nodiscard]] Timestamp pollReturnTime() const { return pollReturnTime_; }
+
+		//在当前loop中执行回调
+		void runInLoop(const Functor &cb);
+
+		//把cb放入队列中，唤醒loop所在的线程，执行回调
+		void queueInLoop(const Functor &cb);
+
+		//唤醒loop所在的线程
+		void wakeup();
+
+		void updateChannel(Channel *channel);
+
+		void removeChannel(Channel *channel);
+
+		bool hasChannel(Channel *channel);
+
+		//判断EventLoop对象是否在自己的线程里
+		[[nodiscard]] bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
+
 
 	private:
+		using ChannelList = std::vector<Channel *>;
+
+		//执行回调
+		void doPendingFunctors();
+
+	private:
+		std::atomic_bool looping_{};
+		std::atomic_bool quit_{};
+		std::atomic_bool callingPendingFunctors_{};   //标识当前loop是否有需要执行的回调操作
+		const pid_t threadId_;
+		Timestamp pollReturnTime_;
+		std::unique_ptr<Poller> poller_;
+		int wakeupFd_{};                              //用于eventfd，线程间通信
+		std::unique_ptr<Channel> wakeupChannel_;
+		ChannelList activeChannels_;
+		Channel *currentActiveChannel_{};
+		std::mutex mutex_;                          //保持下面容器线程安全
+		std::vector<Functor> pendingFunctors_;      //存储loop需要执行的所有回调操作
 	};
 }
--- src/net/EventLoop.cpp
+++ src/net/EventLoop.cpp
@@ -1,3 +1,104 @@
 #include "net/EventLoop.hpp"
+#include "net/Poller.hpp"
+#include "net/Channel.hpp"
+#include "net/SocketOps.hpp"
+#include "base/Log.hpp"
+#include <sys/eventfd.h>
 
+namespace {
+//防止一个线程创建多个EventLoop   __thread：thread_local
+//当一个event loop创建起来它就指向那个对象，在一个线程里再去创建一个对象，由于这个指针为空，就不创建
+	__thread cm::net::EventLoop *t_loopInThisThread = nullptr;
+//定义默认的Poller IO复用接口的超时时间
+	const int kPollTimeMs = 10000;//10秒钟
+}
 
+cm::net::EventLoop::EventLoop() : looping_(false), quit_(false), callingPendingFunctors_(false),
+                                  threadId_(CurrentThread::tid()), poller_(Poller::newDefaultPoller(this)),
+                                  wakeupChannel_(std::make_unique<Channel>(this, wakeupFd_)),
+                                  currentActiveChannel_(nullptr) {
+	wakeupFd_ = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+	if (wakeupFd_ < 0) {
+		LOG_FATAL("Failed in eventfd");
+		abort();
+	}
+	if (t_loopInThisThread) {
+		LOG_FATAL("Another EventLoop %p exists in this base %d", t_loopInThisThread, t_loopInThisThread);
+	} else {
+		t_loopInThisThread = this;
+	}
+	//设置wakeup fd的事件类型以及发生事件后的回调操作
+	wakeupChannel_->setReadCallback([&](const Timestamp &) {
+		uint64_t one = 1;
+		ssize_t n = sockets::read(wakeupFd_, &one, sizeof(one));
+		if (n != sizeof(one)) {
+			LOG_ERROR("EventLoop::handleRead() reads %d bytes instead of 8", n);
+		}
+	});
+	//每一个event loop都将监听wakeup channel的EPOLLIN读事件了
+	wakeupChannel_->enableReading();
+}
+
+//开启事件循环 驱动底层的poller执行poll
+void cm::net::EventLoop::loop() {
+	looping_ = true;
+	quit_ = false;
+	while (!quit_) {
+		activeChannels_.clear();
+		//监听两类fd， client fd wake fd
+		pollReturnTime_ = poller_->poll(kPollTimeMs, &activeChannels_);
+		for (auto i: activeChannels_) {
+			//Poller监听哪些channel发生事件了，然后上报给EventLoop，通知channel处理相应的事件
+			i->handleEvent(pollReturnTime_);
+		}
+		/**
+		 * 执行当前EventLoop事件循环需要处理的回调操作
+		 * IO线程mainLoop accept fd <= channel subloop
+		 * mainLoop事先注册一个回调，需要subLoop来执行，wakeup subLoop后，执行下面的方法，执行执行之前mainLoop注册的回调
+		 */
+		doPendingFunctors();
+	}
+}
+
+void cm::net::EventLoop::quit() {
+	quit_ = true;
+	//如果在其他线程中调用，在一个subLoop中调用mainLoop quit，需要先让其工作起来回到while，然后判断quit退出loop
+	if (!isInLoopThread()) {
+		wakeup();
+	}
+}
+
+void cm::net::EventLoop::runInLoop(const cm::net::EventLoop::Functor &cb) {
+
+}
+
+void cm::net::EventLoop::queueInLoop(const cm::net::EventLoop::Functor &cb) {
+
+}
+
+void cm::net::EventLoop::wakeup() {
+
+}
+
+void cm::net::EventLoop::updateChannel(cm::net::Channel *channel) {
+
+}
+
+void cm::net::EventLoop::removeChannel(cm::net::Channel *channel) {
+
+}
+
+bool cm::net::EventLoop::hasChannel(cm::net::Channel *channel) {
+	return poller_->hasChannel(channel);
+}
+
+void cm::net::EventLoop::doPendingFunctors() {
+
+}
+
+cm::net::EventLoop::~EventLoop() {
+	wakeupChannel_->disableAll();
+	wakeupChannel_->remove();
+	::close(wakeupFd_);
+	t_loopInThisThread = nullptr;
+}
